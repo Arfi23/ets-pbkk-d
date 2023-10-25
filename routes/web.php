88b3343edb92895e9603803f5e256207fb228979
Arfi23 <arfifaizilraushani@gmail.com>
2023-10-25 13:47:23 +0700

    Menambahkan route halaman rekam medis

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Record;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RecordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route untuk menampilkan halaman index berisi semua catatan rekaman medis
+Route::get('/record', [RecordController::class, 'index'])->name('record.index');
