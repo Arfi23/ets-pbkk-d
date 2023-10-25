a2159e04121e8cb926017326587bcca4016021f8
Arfi23 <arfifaizilraushani@gmail.com>
2023-10-25 13:28:48 +0700

    Membuat file migration tabel records yang berelasi dengan tabel patients dan doctors

---
+++ database/migrations/2023_10_25_062145_create_records_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('patient_id')->unsigned();
+            $table->bigInteger('doctor_id')->unsigned();
+            $table->text('health_cond');
+            $table->double('temperature', 3, 2);
+            $table->timestamps();
+        });
+
+        Schema::table('records', function (Blueprint $table) {
+            $table->foreign('patient_id')->references('id')->on('patients')
+                ->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('doctor_id')->references('id')->on('doctors')
+                ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('records');
+    }
+};
